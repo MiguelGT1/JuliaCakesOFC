728b37fad888a24bfe30d25c3cc84e74f87dc945
MiguelGT1 <miguel.giannotti07@outlook.com>
2023-04-04 14:52:43 -0300

    taindo

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector(".btn-menu").addEventListener("click", function() {
+   document.querySelector(".menu").style.display = "block";
+ });
+ 
+ document.querySelector(".btn-close").addEventListener("click", function() {
+   document.querySelector(".menu").style.display = "none";
+ });
+ 
