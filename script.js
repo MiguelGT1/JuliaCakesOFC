57d6c5de6beb8b5a282d22dc9bead0e74d4c2cbf
MiguelGT1 <miguel.giannotti07@outlook.com>
2023-04-18 01:24:12 -0300

    page-sobre-add

--- script.js
+++ script.js
@@ -5,4 +5,28 @@ document.querySelector(".btn-menu").addEventListener("click", function() {
  document.querySelector(".btn-close").addEventListener("click", function() {
    document.querySelector(".menu").style.display = "none";
  });
+
+ 
+
+ var paragrafo = document.querySelector('.p-sobre')
+var texto = paragrafo.innerHTML.replace(/<br>/g, '¬')
+var index = 0
+
+const escrever = () => {
+  paragrafo.innerHTML = texto.substring(0, index).replace(/¬/g, '<br>')
+  if(texto.length > index) {
+    paragrafo.innerHTML += '|'
+    index++
+    setTimeout(escrever, 50)
+  } else {
+    paragrafo.innerHTML = paragrafo.innerHTML.replace('|', '')
+  }
+}
+
+escrever()
+
+ 
+
+
+
  
